d6be7a09c5257874f2005d09bef2d9b6fc6332e8
lys <lysbaldrez@icloud.com>
2021-05-16 01:50:57 -0300

    first commit

---
+++ api-holiday/HolidaysTableViewController.swift
@@ -0,0 +1,36 @@
+//
+//  HolidaysTableViewController.swift
+//  api-holiday
+//
+//  Created by Amaryllis Baldrez on 16/05/21.
+//
+
+import UIKit
+
+class HolidaysTableViewController: UITableViewController, UISearchBarDelegate {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        searchBar.delegate = self
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        return cell
+    }
+    
+}
+
---
+++ api-holiday/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  api-holiday
+//
+//  Created by Amaryllis Baldrez on 16/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
